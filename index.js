0d9acb6cb50c3ba8fc0aae85a9c90510fb87eed9
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-05-12 11:19:46 +0300

    3 steps done

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ import _ from 'lodash';
 
 export default function solution(content){
   // BEGIN
-  console.log(content);
+  const rows = content.split('\r\n').slice(1, -1)
+  const data = rows.map(row=>row.split(','))
+  const gamesCount = data.length
+  console.log(`Количество партий: ${gamesCount}`);
+
+  const ratedOrNotList = data.map(el=>el[1])
+  const ratedGamesCount = ratedOrNotList.filter(el=>el==='TRUE').length
+  const unRatedGamesCount = ratedOrNotList.filter(el=>el==='FALSE').length
+  const ratioRate = Math.round((ratedGamesCount / gamesCount) * 100)
+  const ratioUnRate = Math.round((unRatedGamesCount / gamesCount) * 100)
+  
+  console.log(`Соотношение рейтинговых игр к нерейтинговым: ${ratioRate}% к ${ratioUnRate}%`);
+  console.log(unRatedGamesCount);
+  
+  const debuts = data.map(el=>el[13])
+  const sortedDebutes = _.uniq(debuts)
+  console.log(sortedDebutes);
+  
   // END
 }
